d912b3723e993d2a0af65900fe5a0c6106081088
Freddy Kristiansen <fkr@freddy.dk>
2026-09-21 13:46:57 +0200

    Fix azure container registry insights error

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,7 +107,8 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 
   oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+    log_analytics_workspace_id      = azurerm_log_analytics_workspace.this.id
+    msi_auth_for_monitoring_enabled = true
   }
 
   tags = azurerm_resource_group.this.tags
